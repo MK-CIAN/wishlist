6ba877a22b7828b96fd8ea3a8e398b99b0a7c34c
Cian McKenna <cian.mck20@gmail.com>
2024-02-23 11:54:45 +0000

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
-
+import { WishItem } from '../shared/models/wishitem';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  items = [
+    new WishItem('To learn Angular'),
+    new WishItem('Get Coffee', true),
+    new WishItem('Go for a run'),
+  ];
   title = 'wishlist';
 }
