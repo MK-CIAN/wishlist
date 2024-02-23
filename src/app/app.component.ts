16036b2083549fea2002ff3a2308e4a8eb98098e
Cian McKenna <cian.mck20@gmail.com>
2024-02-23 15:00:05 +0000

    WishList Component Created & Errors Fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
 import { WishItem } from '../shared/models/wishitem';
+
+const filters = [
+  (item : WishItem) => item,
+  (item : WishItem) => !item.isComplete,
+  (item : WishItem) => item.isComplete
+];
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -11,5 +18,22 @@ export class AppComponent {
     new WishItem('Get Coffee', true),
     new WishItem('Go for a run')
   ];
+
+  listFilter : any = '0';
+
+  newWishText = '';
+
   title = 'wishlist';
+
+  get visibleItems() : WishItem[] {
+    return this.items.filter(filters[this.listFilter]);
+  }
+
+  addNewWish() {
+    //Adding the new wish to items array
+    //Clear text box
+    this.items.push(new WishItem(this.newWishText));
+    this.newWishText = '';
+  }
+
 }
---
+++ src/app/wish-list/wish-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { WishItem } from '../../shared/models/wishitem';
+
+@Component({
+  selector: 'wish-list',
+  templateUrl: './wish-list.component.html',
+  styleUrl: './wish-list.component.css'
+})
+export class WishListComponent implements OnInit{
+  @Input() wishes : WishItem[] = [];
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  toggleItem(item : WishItem) {
+    item.isComplete = !item.isComplete;
+    console.log(item);
+  }
+}
