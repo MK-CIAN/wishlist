13ed2bda542dfcb30654bf7578733f9b04501ccd
MK-CIAN <cian.mck20@gmail.com>
2024-03-08 14:06:44 +0000

    Wish Module Created

--- src/app/wish.service.ts
+++ src/app/wish.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpHeaders, HttpParams } from '@angular/common/http';
 import { WishItem } from '../shared/models/wishitem';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -24,10 +26,19 @@ export class WishService {
         format: 'json'
       }
     });
-    
-    return this.http.get('assets/wishes.json', options);
+
+    return this.http.get('assets/wishes.json', options).pipe(catchError(this.handleError));
   }
 
+  private handleError(error: HttpErrorResponse) {
+    if (error.status === 0) {
+      console.error('There is and issue with the client or network: ', error.error);
+    } else {
+      console.error('Server-side error: ', error.error);
+    }
+
+    return throwError(() => new Error('Cannot retrieve wishes from the server. Please try again'));
+  }
 
   private addWish(wish: WishItem) {
     let options = this.getStandardOptions();
