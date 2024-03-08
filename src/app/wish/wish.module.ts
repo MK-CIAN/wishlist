c51f8aca88118c940182102bf193f63ddf0253ba
MK-CIAN <cian.mck20@gmail.com>
2024-03-08 14:32:38 +0000

    Wish Component Created

--- src/app/wish/wish.module.ts
+++ src/app/wish/wish.module.ts
@@ -5,14 +5,16 @@ import { AddWishFormComponent } from './add-wish-form/add-wish-form.component';
 import { WishFilterComponent } from './wish-filter/wish-filter.component';
 import { WishListItemComponent } from './wish-list-item/wish-list-item.component';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http'; 
+import { HttpClientModule } from '@angular/common/http';
+import { WishComponent } from './wish.component'; 
 
 @NgModule({
   declarations: [
     WishListComponent,
     AddWishFormComponent,
     WishFilterComponent,
-    WishListItemComponent
+    WishListItemComponent,
+    WishComponent
   ],
   imports: [
     CommonModule,
